f8c9b465aedc4db33adb5db8b8cc9a8231b1f9d8
Sachin-fsd <sachineducational555@gmail.com>
2025-05-31 14:02:12 +0530

    add:username in login

--- backend/routes/login.route.js
+++ backend/routes/login.route.js
@@ -22,7 +22,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: 'Invalid email or password' });
         }
         const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token , ok:true, user: { id: user._id, email: user.email, role: user.role } });
+        res.json({ token , ok:true, user: { id: user._id, email: user.email, role: user.role, username: user.username } });
     } catch (error) {
         res.status(500).json({ message: 'Error logging in', error: error.message });
     }
